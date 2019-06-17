38970cf6001e7a260c01ab16a751ea0c411d0f63
Katana Tran <tkatyk21@gmail.com>
2019-06-17 04:24:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 && int % 5
+    puts fizzbuzz
+  elsif int % 5 
+    puts buzz
+  elsif int % 3
+    puts fizz
+  end
+    
