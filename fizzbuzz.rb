fe074ef84dcc1afb98863f7641ab9246066028fe
Katana Tran <tkatyk21@gmail.com>
2019-06-17 04:31:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if int % 3 && int % 5
+def fizzbuzz(num)
+  if num % 3 && int % 5
     return "FizzBuzz"
   elsif int % 5 
     return "Buzz"
