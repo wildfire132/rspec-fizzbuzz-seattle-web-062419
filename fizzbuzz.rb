af6f4415ea8b04b37932562533dcd7058619d6f5
Katana Tran <tkatyk21@gmail.com>
2019-06-17 04:33:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 && num % 5
+  if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
-  elsif num % 5 
+  elsif num % 5 == 0
     return "Buzz"
-  elsif num % 3
+  elsif num % 3 == 0
     return "Fizz"
   end
   return nil
